2f8500d5b5dcc8e51ab5f2e6f2a597c497b27c7b
RagulKannan2005 <165671801+RagulKannan2005@users.noreply.github.com>
2026-02-01 06:14:17 +0530

    commit

--- saas-frontend/saas/src/Components/Projects/Projects.jsx
+++ saas-frontend/saas/src/Components/Projects/Projects.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Headerpart from "../Dashboard/Headerpart";
+// import { Plane } from "lucide-react";
+import { Plus } from "lucide-react";
+import "./Projects.css";
 
 const Projects = () => {
   return (
     <>
       <Headerpart />
-      <div className="dashboard_content" style={{ padding: "20px" }}>
-        <h1>Projects</h1>
+      <div className="project_content" style={{ padding: "20px" }}>
+        <div className="project_header">
+          <div>
+            <h1>Projects</h1>
+            <p>Manage your projects and teams</p>
+          </div>
+          <div className="new_project_button">
+            <button>
+              <Plus size={20} />
+              New Project
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
